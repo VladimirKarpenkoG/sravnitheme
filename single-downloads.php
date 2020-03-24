a2e6ac3579bcdb2c8124d8a9079e76056a315d02
Vladimir <vk@geroy.ooo>
2020-03-24 16:37:19 +0300

    Changed downloads page layout

--- single-downloads.php
+++ single-downloads.php
@@ -14,29 +14,12 @@ get_header();?>
 	<main class="article-page">
     <article class="article">
       <header class="article__header">
-        <div class="article__header-wrapper">
-          <h1 class="article__title"><?= $post->post_title?></h1>
-          <?php if(false):?>
-            <div class="article__buttons"><a class="btn btn--review" href="javascript:void(0);">К сравнению!</a><a class="btn btn--green btn--download" href="javascript:void(0);"><span class="btn__text">Скачать</span><i class="btn__ico fas fa-arrow-alt-circle-down"></i></a></div>
-          <?php endif;?>
-        </div>
-
-        <div class="article__header-img">
-        <img src="<?= get_field('k8_cmn_illustration') ? wp_get_attachment_image_url( get_field('k8_cmn_illustration'), 'large' ): the_post_thumbnail_url('large') ?>" alt='Иллюстрация статьи "<?= $post->post_title?>"'>
-        </div>
-
+      <h1 class="article__title"><?= $post->post_title?></h1>
       </header>
-      <div class="article__author"><img class="article__author-image" src="<?= get_avatar_url($post->post_author)?>" alt="Автор: <?= get_the_author('',false )?>">
-        <div class="article__author-info">
-          <div class="article__author-create">Опубликовано: 
-            <time datetime="<?= get_the_date() ?>"><?= get_the_date() ?></time>
-          </div>
-          <hr class="article__author-hr">
-          <div class="article__author-name">Автор: <?= get_the_author('',false )?></div>
-        </div>
-      </div>
-        <section class="article__content" style="padding-top: 70px;">
+        <section class="article__content">
+            <div class="info info__table--theme-check">
             <?= the_content(); ?>
+            </div>
             <div class="variant-list variant-list--downloads" style="padding-top: 30px;">
                 <?php foreach($links as $link):?>
                 <div class="variant-list__el"><a class="variant-opts" href="<?=$link['srv_dl_link']?>" rel="nofollow" target="_blank">
