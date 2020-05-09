6c4eae821259352da2f848a7dbe76af73529f33e
Tsvigun Evgeny <t1evgeny93@gmail.com>
2020-05-09 16:28:54 +0300

    added nofollow to liveinternet

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
 <body <?php body_class(); ?>>
 <!--LiveInternet counter--><script type="text/javascript">
 document.write('<a href="//http://www.liveinternet.ru/click" '+
-'target="_blank" class="d-none"><img src="//counter.yadro.ru/hit?t54.1;r'+
+'target="_blank" class="d-none" rel="nofollow"><img src="//counter.yadro.ru/hit?t54.1;r'+
 
 escape(document.referrer)+((typeof(screen)=='undefined')?'':
 
